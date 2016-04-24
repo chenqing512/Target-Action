d0f71d3cc4612d84c1fdf151710eb844ba6c12d5
ChenQing <chenqing@chenqing512.local>
2016-04-24 09:00:01 +0800

    Initial Commit

---
+++ Target-Action/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Target-Action
+//
+//  Created by ChenQing on 16/4/24.
+//  Copyright © 2016年 ChenQing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Target-Action/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Target-Action
+//
+//  Created by ChenQing on 16/4/24.
+//  Copyright © 2016年 ChenQing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
